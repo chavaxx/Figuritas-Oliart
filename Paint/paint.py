9b5228f4c356df0bf942154af8c1a8785f886f8e
itumejia <iturielmg@hotmail.com>
2020-10-29 12:50:22 -0600

    Documentacion

--- Paint/paint.py
+++ Paint/paint.py
@@ -1,45 +1,47 @@
 from turtle import *
 from freegames import vector
-import math
+import math #Librería para cálculos matemáticos
 
+#Función que dibuja una línea
 def line(start, end):
-    "Draw line from start to end."
-    up()
-    goto(start.x, start.y)
-    down()
-    goto(end.x, end.y)
+    up() #Levantar lápiz (no dibujar)
+    goto(start.x, start.y) #Ir a coordenada del primer punto
+    down() #Bajar lápiz (empezar a dibujar)
+    goto(end.x, end.y) #Ir a segundo punto
 
+#Función que dibuja un cuadrado
 def square(start, end):
-    "Draw square from start to end."
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
+    up() #Levantar lápiz (no dibujar)
+    goto(start.x, start.y) #Ir a coordenada del primer punto
+    down() #Bajar lápiz (empezar a dibujar)
+    begin_fill() #Establecemos que se rellenará la figura
 
+    #Ciclo que traslada el lápiz a cada uno de los puntos
     for count in range(4):
         forward(end.x - start.x)
         left(90)
 
-    end_fill()
+    end_fill() #  #Finalizamos el llenado
 
+#Función que dibuja un circulo, el primer punto es el centro y el segundo cualquier punto de la circunferencia
 def circlee(start, end):
-    "Draw circle from start to end."
-    r=math.sqrt(((end.x-start.x)**2)+((end.y-start.y)**2))
-    up()
-    goto(start.x, (start.y-r))
-    down()
-    begin_fill()
-    circle(r)
-    end_fill()
+    r=math.sqrt(((end.x-start.x)**2)+((end.y-start.y)**2)) #Calcular radio
+    up() #Levantar lápiz (no dibujar)
+    goto(start.x, (start.y-r)) #Ir al punto más bajo de la circunferencia (desde ahi empieza a dibujar circle)
+    down() #Bajar lápiz (empezar a dibujar)
+    begin_fill()#Establecemos que se rellenará la figura
+    circle(r) #Se dibuja círculo de radio r
+    end_fill()  #Finalizamos el llenado
 
+#Función que dibuja un rectángulo. Los dos puntos dados son dos de sus esquinas
 def rectangle(start, end):
-    "Draw rectangle from start to end."
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
+    up() #Levantar lápiz (no dibujar)
+    goto(start.x, start.y) #Ir a coordenada del primer punto
+    down() #Bajar lápiz (empezar a dibujar)
+    begin_fill() #Establecemos que se rellenará la figura
 
-    goto(start.x,end.y)
+    #Se recorren los cuatro vértices del rectángulo
+    goto(start.x,end.y) 
     goto(end.x,end.y)
     goto(end.x,start.y)
     goto(start.x, start.y)    
@@ -58,38 +60,42 @@ def triangle(start, end):
 
     end_fill() #Finalizamos el llenado
 
-
+#Guarda información de coordenadas cuando se da click en el canvas
 def tap(x, y):
-    "Store starting point or draw shape."
-
     start = state['start']
 
+    #Si el start es None, se guarda en el la información del click
     if start is None:
         state['start'] = vector(x, y)
+    #Si no, entonces se dibuja la figura, siendo el click la información de end. Al final start se reestablece
     else:
         shape = state['shape']
         end = vector(x, y)
         shape(start, end)
         state['start'] = None
 
+#Guarda el tipo de figuras que se están dibujando
 def store(key, value):
-    "Store value in state at key."
     state[key] = value
 
-state = {'start': None, 'shape': line}
-setup(420, 420, 370, 0)
-onscreenclick(tap)
+state = {'start': None, 'shape': line} #Valores iniciales. Se empieza con linea por default
+setup(420, 420, 370, 0) #Medidas del canva
+onscreenclick(tap) #Llama función tap en cada click
 listen()
-onkey(undo, 'u')
-onkey(lambda: color('black'), 'K')
+onkey(undo, 'u') #Deshacer cuando se presione 'u'
+
+#Cambio de colores cuando se presionen las teclas señaladas
+onkey(lambda: color('black'), 'K') 
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: color('yellow'), 'Y')
+
+#Cambio de figura cuando se presionen las teclas señaladas (se llama store para guardar este valor)
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circlee), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
-done()
+done() #Determina el loop para esperar instrucciones en pantalla
